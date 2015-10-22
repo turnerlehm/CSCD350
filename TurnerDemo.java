fe5072619370c08a11d337b9bb92ba55c9fbb4af
Turner Lehmbecker <edmfrosty@gmail.com>
2015-10-21 20:38:05 -0700

    turner's lab5 thingy
    yeah

---
+++ TurnerDemo.java
@@ -0,0 +1,12 @@
+/*
+*Tuner lab5 test
+*/
+
+public class TurnerDemo
+{
+   public static void main(String[] args)
+   {
+      while(true)
+         System.out.println("huehuehuehuehuehuehuehuehuehuehuehuehuehuehuehuehuehuehuehuehuehuehuehuehuehuehuehue");
+   }
+}
