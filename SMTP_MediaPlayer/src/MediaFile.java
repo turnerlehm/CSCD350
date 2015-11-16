d928310268523a2d6b2245ccc48d3c7486c91d35
Samuel Arutyunyan <turtle.emperor@gmail.com>
2015-11-15 20:49:36 -0800

    Added to database addMedia(), createPlaylist(), addToPlaylist(). Added MediaFile class

--- SMTP_MediaPlayer/src/MediaFile.java
+++ SMTP_MediaPlayer/src/MediaFile.java
@@ -1,4 +1,6 @@
-
+/**
+ * Created by Sam Arutyunyan on 11/2/2015.
+ */
 public class MediaFile
 {
     String filename;
@@ -7,4 +9,70 @@ public class MediaFile
     String artist;
     String directory;
     String genre;
+    public MediaFile(String file_name, String extension, String path)
+    {
+        filename = file_name;
+        ext = extension;
+        directory = path;
+    }
+
+    public String getFilename()
+    {
+        return filename;
+    }
+
+    public void setFilename(String filename)
+    {
+        this.filename = filename;
+    }
+
+    public String getExt()
+    {
+        return ext;
+    }
+
+    public void setExt(String ext)
+    {
+        this.ext = ext;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getArtist()
+    {
+        return artist;
+    }
+
+    public void setArtist(String artist)
+    {
+        this.artist = artist;
+    }
+
+    public String getDirectory()
+    {
+        return directory;
+    }
+
+    public void setDirectory(String directory)
+    {
+        this.directory = directory;
+    }
+
+    public String getGenre()
+    {
+        return genre;
+    }
+
+    public void setGenre(String genre)
+    {
+        this.genre = genre;
+    }
 }
