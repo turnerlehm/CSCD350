b677a2fa1f1b936a8c7c18cd4142f514a6ebcb45
mlutch2010 <mlutch2010@gmail.com>
2015-11-23 02:12:33 -0800

    added flag checking
    Added flag checks to open and play

--- SMTP_MediaPlayer/src/command/commands/CommandOpen.java
+++ SMTP_MediaPlayer/src/command/commands/CommandOpen.java
@@ -1,8 +1,11 @@
 package command.commands;
 
 import command.*;
+
 public class CommandOpen extends AbstractCommand implements Command
 {	
+	private FLAG_TYPE[] Allowed_Flags = new FLAG_TYPE[]{FLAG_TYPE.PLAYLIST, FLAG_TYPE.ARTIST, FLAG_TYPE.NOFLAG};
+	
 	public CommandOpen()
 	{
 		super(COMMAND_TYPE.OPEN);
@@ -10,7 +13,18 @@ public class CommandOpen extends AbstractCommand implements Command
 	
 	public void addFlag(Flag flagToAdd) throws InvalidCommandException
 	{
-		super.addFlag(flagToAdd);
+		boolean added = false;
+		for(int x = 0; x < Allowed_Flags.length; x++)
+		{
+			if(Allowed_Flags[x].equals(flagToAdd._Flag))
+			{
+				super.addFlag(flagToAdd);
+				added = true;
+				break;
+			}
+		}
+		if(!added)
+			throw new InvalidCommandException("Invalid Flag");
 	}
 	
 	public static String help()
--- SMTP_MediaPlayer/src/command/commands/CommandPlay.java
+++ SMTP_MediaPlayer/src/command/commands/CommandPlay.java
@@ -4,6 +4,8 @@ import command.*;
 
 public class CommandPlay extends AbstractCommand implements Command
 {	
+	public FLAG_TYPE[] Allowed_Flags = new FLAG_TYPE[]{FLAG_TYPE.ARTIST, FLAG_TYPE.GENRE, FLAG_TYPE.SONG};
+	
 	public CommandPlay()
 	{
 		super(COMMAND_TYPE.PLAY);
@@ -11,7 +13,18 @@ public class CommandPlay extends AbstractCommand implements Command
 	
 	public void addFlag(Flag flagToAdd) throws InvalidCommandException
 	{
-		super.addFlag(flagToAdd);
+		boolean added = false;
+		for(int x = 0; x < Allowed_Flags.length; x++)
+		{
+			if(Allowed_Flags[x].equals(flagToAdd._Flag))
+			{
+				super.addFlag(flagToAdd);
+				added = true;
+				break;
+			}
+		}
+		if(!added)
+			throw new InvalidCommandException("Invalid Flag");
 	}
 	
 	public static String help()
